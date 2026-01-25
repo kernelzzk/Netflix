0ab6eb004bc1dba68beb53d193ecaf415b953d34
skyward <charlintoxic@gmail.com>
2026-01-24 21:54:03 -0300

    Add new TV channel entries
    Added new TV channel entries with URLs in user.py.

--- user.py
+++ user.py
@@ -1,2 +1,7 @@
 #EXTINF:-1 tvg-id="acesso🌩️" group-title="" tvg-name="Acesso" tvg-logo="https://tomatoanimes.com/_next/image?url=/images/happy-min.png&w=256&q=75",🌩️
 http://2qq.pro/210213270/780923762
+#######################
+
+
+#EXTINF:-1 tvg-id="globosp" group-title="" tvg-name="GLOBO SP" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/1/1f/TV_Globo_logo_(April_2025).png",GLOBO SP
+http://cd.cddclox.store:80/live/manoelpaimanu/981354922/275075.m3u8
